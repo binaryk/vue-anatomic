82b7c025d2dd3a1a0e55c2df6880603b3cd8c885
Cristi Jora <joracristi@gmail.com>
2018-05-08 13:46:16 +0300

    Change from codesandbox
    Change from codesandbox directly

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,18 +6,19 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   data() {
     return {
-      msg: 'Vue anatomic components',
+      msg: "This is a change from codesanbox"
     };
-  },
+  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
